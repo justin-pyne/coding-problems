d51a9901edcaee50a1d2ea5836ee361892978ba6
justin-pyne <jpyne.justin@gmail.com>
2023-03-01 15:27:26 -0700

    initialize repo

---
+++ LC-1-twosum-Loops.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        nums = [2,7,11,15], target = 9
+        return [0, 1]
+        
+        """
+        pass
+        for i, n in enumerate(nums):
+            for j, k in enumerate(nums):
