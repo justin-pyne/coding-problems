bc5aa01f811cea9b4ba3d053424dd83dbfc0db3e
justin-pyne <jpyne.justin@gmail.com>
2023-04-21 18:04:23 +0900

    top K Frequent Elements, dict sort solution

--- leetcode/python/topKFrequentElements_Dict_347.py
+++ leetcode/python/topKFrequentElements_Dict_347.py
@@ -1,3 +1,17 @@
 class Solution:
+    from typing import List
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
+        """
+        create a dict with elements and their count
+        sort the list of elements by their count
+        grab the first k elements when elements are ordered descending
+        """
+
+        from collections import Counter
+        
+        count = Counter(nums)
+        unique_elements = list(count.keys())
+        unique_elements.sort(key=lambda x: count[x], reverse = True)
+
+        return unique_elements[:k]
