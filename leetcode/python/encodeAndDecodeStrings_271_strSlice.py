d86ac6fc8cb9e1493690e344975566fa2a6a4801
justin-pyne <jpyne.justin@gmail.com>
2023-04-23 14:39:27 +0900

    py 271 sol

--- leetcode/python/encodeAndDecodeStrings_271_strSlice.py
+++ leetcode/python/encodeAndDecodeStrings_271_strSlice.py
@@ -3,13 +3,31 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        
+        #var
+        encoded = ""
+
+        for s in strs:
+            encoded += f"{len(s)}#{s}"
+        return encoded
 
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
+        #vars
+        decoded = []
+        i = 0
+
+        #search for delimiter, find length, extract substring
+        while i < len(s):
+            delim = s.find("#", i) # start from i to avoid repeat words
+            length = int(s[i:delim])
+            i = delim+1
+            decoded.append(s[i:i+length])
+            i += length
+        
+        return decoded
         
 
 
