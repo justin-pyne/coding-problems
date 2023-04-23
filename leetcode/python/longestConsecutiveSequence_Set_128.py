4e22eb905314fd80d3cdcef93a78e2db6d4f4922
justin-pyne <jpyne.justin@gmail.com>
2023-04-23 15:30:13 +0900

    py128 set solution

--- leetcode/python/longestConsecutiveSequence_Set_128.py
+++ leetcode/python/longestConsecutiveSequence_Set_128.py
@@ -1,4 +1,21 @@
 class Solution:
     from typing import List
     def longestConsecutive(self, nums: List[int]) -> int:
-        pass
+        """
+        """
+        num_set = set(nums)
+        longest_length = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_length = 1
+                while current_num+1 in num_set:
+                    current_num += 1
+                    current_length += 1
+        
+                longest_length = max(longest_length, current_length)
+        
+        return longest_length
+
+
