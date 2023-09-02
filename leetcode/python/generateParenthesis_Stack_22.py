f13299e69cf32eff695049c25c64f6d2945bda56
justin-pyne <jpyne.justin@gmail.com>
2023-09-02 11:36:08 -0700

    submit LC22

--- leetcode/python/generateParenthesis_Stack_22.py
+++ leetcode/python/generateParenthesis_Stack_22.py
@@ -1,2 +1,27 @@
+from typing import List
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        solution = []
+
+        def backtrack(openNum, closedNum):
+
+            #base case
+            if openNum == closedNum == n:
+                solution.append("".join(stack))
+                return
+            
+            if openNum < n:
+                stack.append('(')
+                backtrack(openNum+1, closedNum)
+                stack.pop()
+            
+            if closedNum < openNum:
+                stack.append(')')
+                backtrack(openNum, closedNum+1)
+                stack.pop()
+            
+        
+        backtrack(0,0)
+        return solution
