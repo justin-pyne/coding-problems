146970babc361e1b129b28087a6ffbcabd91c924
justin-pyne <jpyne.justin@gmail.com>
2023-10-04 14:19:04 -0700

    format + 21py recursive

--- leetcode/python/mergeTwoSortedLists_linkedListIterative_21.py
+++ leetcode/python/mergeTwoSortedLists_linkedListIterative_21.py
@@ -14,7 +14,7 @@ class Solution:
         while list1 and list2:
             if list1.val < list2.val:
                 node.next = list1
-                list1 = list1.nexxt
+                list1 = list1.next
             else:
                 node.next = list2
                 list2 = list2.next
