d853bb5038c042ad266601120242f5f6d1b2906e
justin-pyne <jpyne.justin@gmail.com>
2023-10-11 16:10:03 -0600

    binaryTree right side view BFS

---
+++ leetcode/python/binaryTreeRightSideView_BFS_199.py
@@ -0,0 +1,33 @@
+from typing import Optional, List
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level_len = len(queue)
+
+            for i in range(level_len):
+                node = queue.popleft()
+
+                if i == level_len-1:
+                    result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+
+        return result
