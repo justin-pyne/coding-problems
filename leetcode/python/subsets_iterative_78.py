f13ae72823b06b3d4a22daa8092d552eb7c065d7
justin-pyne <jpyne.justin@gmail.com>
2023-10-13 16:07:01 -0600

    subsets iterative sol

---
+++ leetcode/python/subsets_iterative_78.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        output = [[]]
+
+        for num in nums:
+            output += [curr + [num] for curr in output]
+        
+        return output
