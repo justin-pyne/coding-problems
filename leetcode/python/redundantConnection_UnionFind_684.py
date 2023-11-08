be8d8e13fe2bec1e0f67a855beaf14eedf1f3b44
justin-pyne <jpyne.justin@gmail.com>
2023-11-08 14:12:41 -0700

    init redundant connection

---
+++ leetcode/python/redundantConnection_UnionFind_684.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        
+
+
+        def find(n):
+
+        
+        def union(n1, n2):
